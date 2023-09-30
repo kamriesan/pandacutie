0dcf482826ef1a4617348290c629dcef9c070fd1
Kamriesan <gangoso.mikaella@gmail.com>
2023-09-30 16:05:09 +0800

    first commit

---
+++ act2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Import the dataset.csv data: dataset
+dataset = pd.read_csv('data.csv', header=None, names=["Data"])
+
+# Print out dataset
+# print(dataset)
+
+print(dataset)
+
