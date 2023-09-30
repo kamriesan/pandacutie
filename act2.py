ccf1117033e4b034e65159fe3410ab19542fd294
meowicor <mrcr.macaraig3939@gmail.com>
2023-09-30 23:20:14 +0800

    [MARI] updated gui

--- act2.py
+++ act2.py
@@ -1,45 +1,28 @@
-import tkinter as tk
-from tkinter import ttk
+import csv
+import sys
 import pandas as pd
-
-# Initialize empty lists to store data for each category
-names = []
-birthdays = []
-phones = []
-emails = []
-
-# Simulated data for demonstration
-# Populate the lists from the simulated data
-data = {
-    "Name": names,
-    "Birthday": birthdays,
-    "Phone": phones,
-    "Email": emails
-}
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeView
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtGui import QFont, QColor  # Import QFont and QColor
 
 # Read the data from the CSV file
+names, birthdays, phones, emails = [], [], [], []
 with open('data.csv', 'r') as file:
-    lines = file.readlines()
-    current_category = None
-
-    for line in lines:
-        line = line.strip()
-        
-        # Check for the presence of specific characters to categorize data
-        if '@' in line:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        if '@' in row[0]:
             current_category = "Email"
-            emails.append(line)
-        elif '/' in line:
+            emails.append(row[0])
+        elif '/' in row[0]:
             current_category = "Birthday"
-            birthdays.append(line)
-        elif any(char.isdigit() for char in line):
+            birthdays.append(row[0])
+        elif any(char.isdigit() for char in row[0]):
             current_category = "Phone"
-            phones.append(line)
+            phones.append(row[0])
         else:
             current_category = "Name"
-            names.append(line)
+            names.append(row[0])
 
-# Update the data dictionary with the populated lists
 data = {
     "Name": names,
     "Birthday": birthdays,
@@ -49,31 +32,56 @@ data = {
 
 df = pd.DataFrame(data)
 
-# Create the main application window
-root = tk.Tk()
-root.title("Joelflix Employees Database")
+class DataFrameViewer(QMainWindow):
+    def __init__(self, df):
+        super().__init__()
+        self.df = df
+        self.initUI()
 
-# Create a treeview widget
-tree = ttk.Treeview(root)
+    def initUI(self):
+        self.setWindowTitle('Joelflix Employees Database')
+        self.setGeometry(500, 100, 850, 840)
 
-# Define columns
-columns = ['Index'] + list(df.columns)  # Add an 'Index' column at the start
-tree["columns"] = columns
+        # Stylesheet for a formal blue appearance
+        stylesheet = """
+            QHeaderView::section {
+                padding: 5px;
+                border: 0px solid #dcdcdc;
+            }
+        """
+        self.setStyleSheet(stylesheet)
 
-# Configure column headings
-for col in columns:
-    tree.heading(col, text="" if col == "Index" else col, anchor="w")
-    if col == "Index":
-        tree.column(col, width=50)  # Set a smaller width for the 'Index' column
+        # Create a standard item model
+        model = QStandardItemModel(self.df.shape[0], self.df.shape[1] + 1)  # Adjusted for the index column
 
-# Insert data into the treeview with an index starting from 1
-for i, row in df.iterrows():
-    values = [i + 1] + list(row.values)
-    tree.insert("", "end", iid=i, values=values)
+        # Set column headers, including the index header
+        model.setHorizontalHeaderItem(0, QStandardItem())
+        for col_idx, header in enumerate(self.df.columns):
+            header_item = QStandardItem(header)
+            header_item.setFont(QFont("Inter", 9, QFont.Bold))  # Set font to Inter and bold
+            model.setHorizontalHeaderItem(col_idx + 1, header_item)
+        
+        # Populate the model with DataFrame values
+        for row_idx in range(self.df.shape[0]):
+            model.setItem(row_idx, 0, QStandardItem(str(row_idx + 1)))  # Set the index starting from 1
+            for col_idx in range(self.df.shape[1]):
+                item = QStandardItem(str(self.df.iat[row_idx, col_idx]))
+                # Set a background color for every other row
+                if row_idx % 2 == 0:
+                    item.setBackground(QColor('#E6E6E6'))  # darkey gray background
+                elif row_idx % 2 == 1:
+                    item.setBackground(QColor('#F6F6F6'))  # lighter gray background
+                model.setItem(row_idx, col_idx + 1, item)
 
-# Remove the blank column on the left
-tree["show"] = "headings"
+        # Create a tree view and set the model
+        tree_view = QTreeView(self)
+        tree_view.setModel(model)
+        tree_view.resizeColumnToContents(0)  # Adjust the column index accordingly
+        tree_view.setColumnWidth(1, 200)
+        self.setCentralWidget(tree_view)
 
-tree.pack(padx=20, pady=10, fill="both", expand=True)
-
-root.mainloop()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    viewer = DataFrameViewer(df)
+    viewer.show()
+    sys.exit(app.exec_())
