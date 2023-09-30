633064fa45e074e55d0413cbec78feab902028ed
Kamriesan <gangoso.mikaella@gmail.com>
2023-09-30 16:15:43 +0800

    [Mika] basic table lang

--- act2.py
+++ act2.py
@@ -1,10 +1,41 @@
 import pandas as pd
 
-# Import the dataset.csv data: dataset
-dataset = pd.read_csv('data.csv', header=None, names=["Data"])
+# Initialize empty lists to store data for each category
+names = []
+birthdays = []
+phones = []
+emails = []
 
-# Print out dataset
-# print(dataset)
+# Read the data from the CSV file
+with open('data.csv', 'r') as file:
+    lines = file.readlines()
+    current_category = None
 
-print(dataset)
+    for line in lines:
+        line = line.strip()
+        
+        # Check for the presence of specific characters to categorize data
+        if '@' in line:
+            current_category = "Email"
+            emails.append(line)
+        elif '/' in line:
+            current_category = "Birthday"
+            birthdays.append(line)
+        elif any(char.isdigit() for char in line):
+            current_category = "Phone"
+            phones.append(line)
+        else:
+            current_category = "Name"
+            names.append(line)
 
+# Create a DataFrame from the categorized data
+data = {
+    "Name": names,
+    "Birthday": birthdays,
+    "Phone": phones,
+    "Email": emails
+}
+df = pd.DataFrame(data)
+
+# Print the resulting DataFrame
+print(df)
